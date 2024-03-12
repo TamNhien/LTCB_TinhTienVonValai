78fe2a5084f13348b8fb42db5962715a962b05eb
TamNhien <canhthien91@gmail.com>
2024-03-12 11:08:21 +0700

    Add project files.

---
+++ LTCB_TinhTienVonVaLai.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	float T;
+	cout << "Nhap so tien gui (trieu dong) : "; cin >> T;
+
+	float von = T;
+	for (int i = 1; i <= 10; ++i) {
+		float lai = von * 0.07; // Lãi sau mỗi năm
+		von += lai;
+		cout << "Nam " << i << ": VON = " << von << " trieu dong,LAI = " << lai << " trieu dong " << endl;
+	}
+	return 0;
+}
+
+
